ade2b8770251d83afe5f638e92c716b607d1ee8b
SherifZekryWebsites <prosherifzekry@gmail.com>
2019-03-10 10:25:03 +0200

    Initial Commit

---
+++ ZoomMe/ZoomMe.h
@@ -0,0 +1,19 @@
+//
+//  ZoomMe.h
+//  ZoomMe
+//
+//  Created by Sherif  Wagih on 3/10/19.
+//  Copyright © 2019 Sherif  Wagih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZoomMe.
+FOUNDATION_EXPORT double ZoomMeVersionNumber;
+
+//! Project version string for ZoomMe.
+FOUNDATION_EXPORT const unsigned char ZoomMeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZoomMe/PublicHeader.h>
+
+
